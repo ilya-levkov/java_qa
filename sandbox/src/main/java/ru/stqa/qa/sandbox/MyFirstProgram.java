910c4431f54016bfbcbe733ab9f32fd72d57f672
ilya-levkov <ilya.levkov2@gmail.com>
2018-02-11 20:39:14 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/qa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/qa/sandbox/MyFirstProgram.java
@@ -5,8 +5,32 @@ public class MyFirstProgram {
 		String somebody = "world";
 		System.out.println("Hello, " +  somebody + "!");
 
-		double l = 8.0;
-		double s = l * l;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+		//double l = 8.0;
+		//double s = l * l;
+		//System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+		hello("world");
+		hello("user");
+		hello("Ilya");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static  double area(double a, double b) {
+		return a * b;
 	}
 }
